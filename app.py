6df4dec3e47cbef049761f5021072e8c28fda2d1
Миша Мишин <a@mail.ru>
2025-01-14 20:18:46 +0300

    Написал footer

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return render_template('base.html')
+    year = datetime.now().year
+    return render_template('base.html', year=year)
 
-
-# test
 if __name__ == '__main__':
     app.run(debug=True)
